c0996a8347b49e8a5eca9dfaf95176c282a0362b
Vagarth <vagarth419@gmail.com>
2024-10-16 20:38:14 +0530

    feat: create account for poll

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -8,63 +8,43 @@ declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 pub mod voting {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseVoting>) -> Result<()> {
-    Ok(())
-  }
+    pub fn initialize_poll (ctx: Context<InitializePoll>, poll_id: u64, poll_start: u64, poll_end: u64, description: String) -> Result<()> {
+      let poll = &mut ctx.accounts.poll;
+      poll.poll_id = poll_id;
+      poll.description = description;
+      poll.poll_start = poll_start;
+      poll.poll_end = poll_end;
+      poll.candidate_number = 0;
+      Ok(())
+    }
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.voting.count = ctx.accounts.voting.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.voting.count = ctx.accounts.voting.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeVoting>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.voting.count = value.clone();
-    Ok(())
-  }
 }
 
 #[derive(Accounts)]
-pub struct InitializeVoting<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+#[instruction(poll_id: u64)]
+pub struct InitializePoll<'info> {
 
-  #[account(
-  init,
-  space = 8 + Voting::INIT_SPACE,
-  payer = payer
-  )]
-  pub voting: Account<'info, Voting>,
+  #[account(mut)]
+  pub signer: Signer<'info>,
   pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseVoting<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
 
   #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
+    init,
+    payer = signer,
+    space = 8 + Poll::INIT_SPACE,
+    seeds = [poll_id.to_le_bytes().as_ref()],
+    bump
   )]
-  pub voting: Account<'info, Voting>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub voting: Account<'info, Voting>,
+  pub poll: Account<'info, Poll>
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Voting {
-  count: u8,
+pub struct Poll {
+  pub poll_id: u64,
+  #[max_len(100)]
+  pub description: String,
+  pub poll_start: u64,
+  pub poll_end: u64,
+  pub candidate_number: u64
 }
